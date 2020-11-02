98f8983a9974dda3c4d49347ffed3abba45c1cd8
Hugofgx9 <hugo.faugeroux@mmibordeaux.com>
2020-11-02 12:10:19 +0100

    hide router link active on desktop

--- src/views/mobileViews/MobileHome.vue
+++ src/views/mobileViews/MobileHome.vue
@@ -1,9 +1,11 @@
 <script>
 import OvalLink from '@/components/OvalLink.vue';
+import InfiniteScroll from '@/components/InfiniteScroll.vue';
 export default {
 	name: 'MobileHome',
 	components: {
 		OvalLink,
+		InfiniteScroll,
 	},
 	data() {
 		return {
@@ -18,18 +20,23 @@ export default {
 
 <template>
 	<div id='home'>
-		<p class='paragraph-1'>Hi, <br/>
-			My name is <span class='italic-text'> Louise Peredes Fantozzi </span>
-		</p>
-		<p class='paragraph-2'>
-			I'm <span class='italic-text'> French </span> student in the <br/> 
-			<span class='italic-text'> DUT Information Communication </span> <br/>
-			speciality <span class='italic-text'> Advertising </span> and <br/>
-			I'm passionate <br/>
-			about <span class='italic-text'> Graphic Design </span> <br/>
-		</p>
-		<div class="button-container">
-			<oval-link class="view-my-work" text="View my work" to="/" :myStyle="this.ovalStyle"/>
+		<div class="first-part">
+			<p class='paragraph-1'>Hi, <br/>
+				My name is <span class='italic-text'> Louise Peredes Fantozzi </span>
+			</p>
+			<p class='paragraph-2'>
+				I'm <span class='italic-text'> French </span> student in the <br/> 
+				<span class='italic-text'> DUT Information Communication </span> <br/>
+				speciality <span class='italic-text'> Advertising </span> and <br/>
+				I'm passionate <br/>
+				about <span class='italic-text'> Graphic Design </span> <br/>
+			</p>
+			<div class="button-container">
+				<oval-link class="view-my-work" text="View my work" to="/" :myStyle="this.ovalStyle"/>
+			</div>
+		</div>
+		<div id="works">
+			<InfiniteScroll/>
 		</div>
 
 	</div>
@@ -38,26 +45,32 @@ export default {
 <style lang='scss' scoped>
 
 #home{
-	font-size: $big-font-mobile;
-	display: grid;
-	height: 80vh;
-	grid-template-rows: 1fr 1fr 1fr;
-	align-items: center;
+	.first-part {
+		font-size: $big-font-mobile;
+		display: grid;
+		height: 80vh;
+		grid-template-rows: 1fr 1fr 1fr;
+		align-items: center;
 
-	.paragraph-1 {
-	}
-
-	.paragraph-2 {
-		text-align: right;
-		align-self: start;
-	}
-
-	.button-container {
-		.view-my-work{
-			position: absolute;
-			right: -4vw;
-			font-size: 3.5vw;
+		.paragraph-1 {
 		}
+
+		.paragraph-2 {
+			text-align: right;
+			align-self: start;
+		}
+
+		.button-container {
+			.view-my-work{
+				position: absolute;
+				right: -4vw;
+				font-size: 3.5vw;
+			}
+		}
+	}
+
+	#works {
+		height: 200px;
 	}
 }
 	
