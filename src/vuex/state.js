ef37967bb4b3586d5c650037bb07c78ff8a64d14
Hugofgx9 <hugo.faugeroux@mmibordeaux.com>
2020-11-22 17:52:06 +0100

    workpage router param from number to title

--- src/vuex/state.js
+++ src/vuex/state.js
@@ -1,21 +1,21 @@
 const state = {
 	windowWidth: window.innerWidth,
-	works: [
-		{
+	works: {
+		first: {
 			title: 'First Article',
 			software: ['apple', 'mac'],
 			description: 'this is my new project',
 			img1: '',
 			img2: '', 
 		},
-		{
+		second: {
 			title: 'Deuxieme Projet',
 			software: ['apple', 'mac'],
 			description: 'this is my second project',
 			img1: '',
 			img2: '', 
 		},
-	]
+	}
 };
 
 export { state };
