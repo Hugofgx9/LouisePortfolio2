6980cf117ed6778525b62d31d6eb7f5c67c6fdd5
Hugofgx9 <hugo.faugeroux@mmibordeaux.com>
2020-11-22 17:45:51 +0100

    ADD gsap scrolltrigger on workpage about section

--- src/views/desktopViews/WorkPage.vue
+++ src/views/desktopViews/WorkPage.vue
@@ -1,5 +1,8 @@
 <script>
 import { mapState } from 'vuex';
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 export default {
 	name: 'WorkPage',
@@ -8,53 +11,80 @@ export default {
 		}
 	},
 	mounted () {
+		ScrollTrigger.create({
+			trigger:'.about-list',
+			start:'top top+=24px',
+			endTrigger: "about-my-work",
+			end: "bottom bottom",
+			pin: true,
+			scrub: 1,
+		});
 	},
-	computed: mapState([
-		'works'
-	]),
+	methods: {
+	},
+	computed:  {
+		...mapState(['works']),
+		work: function() {
+			return this.works[this.$route.params.id];
+		}
+	},
 }
 </script>
 
 <template>
-	<div id="my-work">
+	<div id="my-work" v-if="work">
 
-		<h2> {{ works[0].title }} </h2>
+		<h2> {{ work.title }} </h2>
 
-		<div class="about-my-work">
-			<ul class="about-list">
-				<li class="software">
-					<h3>SoftWare</h3>
-					<ul class="about-item">
-						<li v-for="software in works[0].software" :key="software"> 
-							{{ software }}
-						</li>
-					</ul>
-				</li>
-				<li class="project">
-					<h3>Project</h3>
-					<div class="about-item">
-						<p> {{ works[0].description }} </p>
-					</div>
-				</li>
-			</ul>
+		<div class="container">
+
+			<div class="illustration-section">
+				<img class="illustration-1" src="https://drive.google.com/uc?id=1Do8pXLqcAhhcMHCB8HHQamZXpHPTtNTr" alt=""/>
+				<img class="illustration-2" src="https://drive.google.com/uc?id=1Do8pXLqcAhhcMHCB8HHQamZXpHPTtNTr" alt=""/>
+			</div>
+
+			<div class="about-my-work">
+				<ul class="about-list">
+					<li class="software">
+						<h3>SoftWare</h3>
+						<ul class="about-item">
+							<li v-for="software in work.software" :key="software"> 
+								{{ software }}
+							</li>
+						</ul>
+					</li>
+					<li class="project">
+						<h3>Project</h3>
+						<div class="about-item">
+							<p> 
+								{{ work.description }} 
+							</p>
+						</div>
+					</li>
+				</ul>
+			</div>
 
 		</div>
 
-		<div>
-			<img src="" alt=""/>
-			<img src="" alt=""/>
-		</div>
 	</div>
 </template>
 
 <style lang='scss' scoped>
 
 #my-work {
-	display: grid;
-	grid-template-columns: 1fr 405px;
 
-	.about-my-work {
-		grid-column: 2;
+	.container {
+		display: grid;
+		grid-template-columns: 1fr 405px;
+
+		.about-my-work {
+			grid-column: 2;
+			//height: fit-content;
+			//background-color: red;
+
+			ul {
+				//background-color: blue;
+			}
 
 			h3 {
 				font-weight: normal;
@@ -67,8 +97,26 @@ export default {
 				margin-top: 18px;
 				margin-bottom: 18px;
 			}
+		}
 
+		.illustration-section {
+			margin-right: 10vw;
+			position: relative;
+
+			.illustration-2, .illustration-2 {
+				width: 100%;
+				height: auto;	
+			}
+
+			.illustration-1 {
+				position: relative;
+				width: calc(100% + #{$global-padding});
+				margin-left: -$global-padding;
+			}
+		}
 	}
+
+
 }
 
 </style>
--- src/vuex/state.js
+++ src/vuex/state.js
@@ -7,7 +7,14 @@ const state = {
 			description: 'this is my new project',
 			img1: '',
 			img2: '', 
-		}
+		},
+		{
+			title: 'Deuxieme Projet',
+			software: ['apple', 'mac'],
+			description: 'this is my second project',
+			img1: '',
+			img2: '', 
+		},
 	]
 };
 
